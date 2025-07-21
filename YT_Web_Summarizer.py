957d5bb33b76c3ff74b1e30ae889396a7aec93bd
Rohan Bhardwaj <122192870+RohanBhardwaj10@users.noreply.github.com>
2025-07-21 10:05:26 +0530

    Update YT_Web_Summarizer.py

--- YT_Web_Summarizer.py
+++ YT_Web_Summarizer.py
@@ -19,7 +19,7 @@ st.subheader("Summarize URL")
 import os
 from dotenv import load_dotenv
 load_dotenv()
-groq_api_key=os.getenv("GROQ_API_KEY")
+groq_api_key=st.secrets["GROQ_API_KEY"]
 llm=ChatGroq(groq_api_key=groq_api_key,model_name="Llama3-8b-8192")
 
 prompt_gen='''
